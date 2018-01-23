01f1e42811b2c2ae9a1419f88bcaab020e551b64
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-01-23 10:43:50 -0700

    Started the hero asset with ideas for the fuctions

---
+++ Side Scrolling Game/Assets/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//int startingLife = 10;
+		//int startingLevel = 1;
+		//int startingDamage = 2;
+		//int speed = 1;
+		//powerup boost speed
+		//jump- goes up then down maybe in a function
+		//if cealing what happens
+		//break down into simple thinking
+		//jump up does it hit  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//function to get status of health
+
+
+	//fuction to move side to side
+
+
+	//function to jump
+
+
+	//on right arrow(move to the right)
+
+
+	//on up arrow(jump then come down)
+
+
+	//on colision?
+
+
+	
+}
